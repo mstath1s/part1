e0f7d0d3080b4c78dec421be53caccb6cbac999f
Meletis <meletis.stathis@cuberm.com>
2022-10-18 22:26:57 +0300

    First exercices with components in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const Header = (props)=>{
+return(
+  <div> <h1>{props.course}</h1></div>
+
+)
+}
+const Content = (props)=>{
+  return(
+  <p>{props.part} {props.exe}</p>
+  )
+
+}
+const Total = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Number of exercises {props.exercise}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content part = {part1} exe = {exercises1}/>
+      <Content part = {part2} exe = {exercises2}/>
+      <Content part = {part3} exe = {exercises3}/>
+      <Total exercise = {exercises1+exercises2+exercises3}/>
+
     </div>
-  );
+  )
 }
 
 export default App;
