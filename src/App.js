ae02a13e925ee5a0632a823366f16f30416b0c27
Meletis <meletis.stathis@cuberm.com>
2022-10-21 19:22:13 +0300

    1.12 anecdotes step1:Pick random anecdotes

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ const Statistics = (props) => {
   }
 }
 
+
 //save clicks of each button to its own state
 const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
 
@@ -53,6 +54,17 @@ const App = () => {
     analytics: 'Statistics'
   }
 
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+
   const goodCount = () => setGood(good + 1)
   const neutralCount = () => setNeutral(neutral + 1)
   const badCount = () => setBad(bad + 1)
@@ -61,16 +73,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [selected, setSelected] = useState(0)
+  const selectedCount = () => setSelected(Math.floor(Math.random() * anecdotes.length))
   return (
     <div>
-      <Header headerText={Info.header} />
-      <Button onClick={goodCount} text='Good' />
+      { anecdotes[selected] }
+      {console.log(selected)}
+      <p><Button onClick={selectedCount} text='next anecdode' /></p>
+   
+
+      {/* <Button onClick={goodCount} text='Good' />
       <Button onClick={neutralCount} text='Neutral' />
       <Button onClick={badCount} text='Bad' />
       <Header headerText={Info.analytics} />
 
-      <Statistics bad={bad} good={good} neutral={neutral} />
-    </div>
+      <Statistics bad={bad} good={good} neutral={neutral} /> */}
+   </div>
   )
 }
 
