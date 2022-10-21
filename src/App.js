200de73a9df6712e3b26becb8ee24a43c4c45d7a
Meletis <meletis.stathis@cuberm.com>
2022-10-21 12:37:58 +0300

    unicafe 1.8 refactor to display statitics in a components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ const Header = (props) => {
     <div> <h1>{props.headerText}</h1></div>
   )
 }
-
 const Statistic = (props) => {
+  return (<div>{props.statisticText} : {props.count}</div>)
+}
+
+
+const Statistics = (props) => {
+  const all = () => { return (props.bad + props.good + props.neutral) }
+  // (good: 1, neutral: 0, bad: -1)
+  const average = () => { return ((props.bad * (-1) + props.good) / all()) }
+  const positiveFeedbackPercent = () => { return ((props.good) / all() * 100 + '%') }
+
   return (
-    <div>{props.statisticText} : {props.count}</div>
+    <div>
+      <Statistic statisticText='Good' count={props.good} />
+      <Statistic statisticText='Neutral' count={props.neutral} />
+      <Statistic statisticText='Bad' count={props.bad} />
+      <Statistic statisticText='All' count={all()} />
+      <Statistic statisticText='Average' count={average()} />
+      <Statistic statisticText='Positive' count={positiveFeedbackPercent()} />
+    </div>
   )
 }
 
 //save clicks of each button to its own state
-const Button = ({onClick, text}) => <button onClick = {onClick}> {text} </button>
+const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
 
 const App = () => {
 
@@ -24,33 +40,26 @@ const App = () => {
     analytics: 'Statistics'
   }
 
-  const goodCount = ()=>setGood(good+1)
-  const neutralCount = ()=>setNeutral(neutral+1)
-  const badCount = ()=>setBad(bad+1)
+  const goodCount = () => setGood(good + 1)
+  const neutralCount = () => setNeutral(neutral + 1)
+  const badCount = () => setBad(bad + 1)
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all=()=>{return(bad+good+neutral)}
-  // (good: 1, neutral: 0, bad: -1)
-  const average=()=>{return((bad*(-1)+good)/all())}
-  const positiveFeedbackPercent=()=>{return((good)/all()*100+'%')}
+
 
 
   return (
     <div>
-      <Header headerText={Info.header}/>
-      <Button onClick={goodCount} text='Good'/>
-      <Button onClick={neutralCount} text='Neutral'/>
-      <Button onClick={badCount} text='Bad'/>
-      <Header headerText={Info.analytics}/>
-      <Statistic statisticText='Good' count={good}/>
-      <Statistic statisticText='Neutral' count={neutral}/>
-      <Statistic statisticText='Bad' count={bad}/>
-      <Statistic statisticText='All' count={all()}/>
-      <Statistic statisticText='Average' count={average()}/>
-      <Statistic statisticText='Positive' count={positiveFeedbackPercent()}/>
+      <Header headerText={Info.header} />
+      <Button onClick={goodCount} text='Good' />
+      <Button onClick={neutralCount} text='Neutral' />
+      <Button onClick={badCount} text='Bad' />
+      <Header headerText={Info.analytics} />
+      <Statistics bad={bad} good={good} neutral={neutral}/>
+
     </div>
   )
 }
