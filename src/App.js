2d2c992caf99a084c3235b9c9d6e49cfa80f372c
Meletis <meletis.stathis@cuberm.com>
2022-10-21 22:12:26 +0300

    Hmm puzzle

--- src/App.js
+++ src/App.js
@@ -42,7 +42,37 @@ const Statistics = (props) => {
     )
   }
 }
+const anecdotes = [
+  {
+    text: 'If it hurts, do it more often.',
+    votes: 0
+  },
+  {
+    text: 'Adding manpower to a late software project makes it later!',
+    votes: 0
+  },
+  {
+    text: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    votes: 0
+  },
+  {
+    text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    votes: 0
+  },
+  {
+    text: 'Premature optimization is the root of all evil.',
+    votes: 0
+  },
+  {
+    text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    votes: 0
+  },
 
+  {
+    text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    votes: 0
+  }
+]
 
 //save clicks of each button to its own state
 const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
@@ -54,34 +84,47 @@ const App = () => {
     analytics: 'Statistics'
   }
 
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
 
 
-  const goodCount = () => setGood(good + 1)
+  const votesCount = () => {
+   // setVotes(vote + 1)
+   anecdotes[selected].votes = vote
+    setVotes(vote + 1)
+   console.log(vote)
+    //console.log(selected)
+    console.log( anecdotes[selected].votes)
+  }
+
   const neutralCount = () => setNeutral(neutral + 1)
   const badCount = () => setBad(bad + 1)
 
-  const [good, setGood] = useState(0)
+  const [vote, setVotes] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const [selected, setSelected] = useState(0)
-  const selectedCount = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const selectedCount = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+    if(anecdotes[selected].votes > 0)
+    {
+      setVotes(anecdotes[selected].votes)
+    }
+    else{
+      setVotes(0)
+    }
+  }
   return (
     <div>
-      { anecdotes[selected] }
+      {anecdotes[selected].text}
       <br />
-      {console.log(selected)}
+
+      {'has ' + anecdotes[selected].votes + ' votes'}
+      <br />
+      <Button onClick={votesCount} text='vote' />
+
       <Button onClick={selectedCount} text='next anecdode' />
-   
+
 
       {/* <Button onClick={goodCount} text='Good' />
       <Button onClick={neutralCount} text='Neutral' />
@@ -89,9 +132,9 @@ const App = () => {
       <Header headerText={Info.analytics} />
 
       <Statistics bad={bad} good={good} neutral={neutral} /> */}
-   </div>
+    </div>
   )
 }
 
 export default App;
-//Math.floor(Math.random() * lengthOfArray-1);
+
