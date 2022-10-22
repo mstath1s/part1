96de2ffd76423062edf7b3ee8d7f146f9f509374
mstath1s <meletis.stathis@gmail.com>
2022-10-22 18:55:46 +0300

    1.14 unicafe step 3
    Meletis
    
    
    wrong
    
    
    Cleaning up

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ const mostVotedAnecdote = (props)=>{
   return item;
 }
 
-
 //save clicks of each button to its own state
 const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
 
