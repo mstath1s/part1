df40c4abed2f0d54bb10538e769499306d7cdcda
mstath1s <meletis.stathis@gmail.com>
2022-10-22 18:49:08 +0300

    New line test

--- src/App.js
+++ src/App.js
@@ -49,6 +49,8 @@ const mostVotedAnecdote = (props)=>{
   return item;
 }
 
+
+
 //save clicks of each button to its own state
 const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
 
